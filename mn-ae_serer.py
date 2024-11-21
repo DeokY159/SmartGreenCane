a23e2ba789209fe0748756c48001084a9e7c8df5
DeokY159 <hdy00955@gmail.com>
2024-11-21 14:26:25 +0900

    Add http request to IN-CSE

--- mn-ae_serer.py
+++ mn-ae_serer.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import subprocess
 import os
+import requests
 
 app = Flask(__name__)
 impact_received_value = None
@@ -9,7 +10,7 @@ coordinates_log = []
 shock_log=[]
 parse_gps_path = "./parse_gps.py"
 judge_shock_path="./judge_shock.py"
-
+IN_CSE_URL="http://127.0.0.1:3000"
 
 # 임팩트 데이터를 받는 엔드포인트
 @app.route('/post_impact', methods=['POST'])
@@ -20,6 +21,21 @@ def receive_impact_value():
         impact_received_value = data['random_value']
         timestamp = data['timestamp']
         print(f"Received impact random value: {impact_received_value} at {timestamp}")
+
+        impact_container=f"{IN_CSE_URL}/TinyIoT/cane1/impact"
+        headers = {
+            "Content-Type": "application/json",
+            "X-M2M-RVI": "3",
+            "X-M2M-Origin": "CAdmin"
+        }
+        payload = {'impact_value': impact_received_value, 'timestamp': timestamp}
+            
+        response = requests.post(impact_container, headers=headers, json=payload)
+        if response.ok:
+            print(f"Successfully sent impact data to {IN_CSE_URL}.")
+        else:
+            print(f"Failed to send impact data to {IN_CSE_URL}: {response.status_code}, {response.text}")
+
         try:
             result = subprocess.run(
                 ['python', judge_shock_path, str(impact_received_value), str(timestamp)],
@@ -28,10 +44,24 @@ def receive_impact_value():
             raw_judge_data = result.stdout.strip()
             judge_data = raw_judge_data.strip("()").split(",")
             impact_received_value, shock_flag, timestamp = int(judge_data[0]), int(judge_data[1]), judge_data[2]
+            #timestamp=timestamp.lstrip()
+            
             if shock_flag==1:
                 shock_log.append({'shock_flag': shock_flag, 'timestamp': timestamp})
                 print(f"Received shock at timestamp: {timestamp}")
             print("shocked", shock_log)
+            SHOCK_container=f"{IN_CSE_URL}/TinyIoT/becane/shock"
+            headers = {
+                "Content-Type": "application/json",
+                "X-M2M-RVI": "3",
+                "X-M2M-Origin": "CAdmin"
+            }
+            payload = {'shock_flag': shock_flag, 'timestamp': timestamp}
+            response = requests.post(SHOCK_container, headers=headers, json=payload)
+            if response.ok:
+                print(f"Successfully sent shock data to {IN_CSE_URL}.")
+            else:
+                print(f"Failed to send shock data to {IN_CSE_URL}: {response.status_code}, {response.text}")
             return jsonify({'status': 'Impact value received'}), 200
         except subprocess.CalledProcessError as e:
             print(f"Error executing judge_shock.py: {e}")
@@ -54,7 +84,23 @@ def receive_power_value():
     data = request.get_json()
     if 'power_level' in data:
         power_received_value = data['power_level']
+        status = "on" if power_received_value > 0 else "off"
         print(f"Received power value: {power_received_value}")
+        
+        battery_container=f"{IN_CSE_URL}/TinyIoT/cane1/battery"
+        headers = {
+            "Content-Type": "application/json",
+            "X-M2M-RVI": "3",
+            "X-M2M-Origin": "CAdmin"
+        }
+        payload = {'power_level': power_received_value, 'status': status}
+            
+        response = requests.post(battery_container, headers=headers, json=payload)
+        if response.ok:
+            print(f"Successfully sent power data to {IN_CSE_URL}.")
+        else:
+            print(f"Failed to send power data to {IN_CSE_URL}: {response.status_code}, {response.text}")
+               
         return jsonify({'status': 'Power value received'}), 200
     else:
         return jsonify({'error': 'No power_level provided'}), 400
@@ -75,6 +121,20 @@ def receive_coordinates():
     if 'latitude' in data and 'longitude' in data:
         latitude = data['latitude']
         longitude = data['longitude']
+
+        gps_container=f"{IN_CSE_URL}/TinyIoT/cane1/gps"
+        headers = {
+            "Content-Type": "application/json",
+            "X-M2M-RVI": "3",
+            "X-M2M-Origin": "CAdmin"
+        }
+        payload = {'latitude': latitude, 'longitude': longitude}
+            
+        response = requests.post(gps_container, headers=headers, json=payload)
+        if response.ok:
+            print(f"Successfully sent GPS data to {IN_CSE_URL}.")
+        else:
+            print(f"Failed to send GPS data to {IN_CSE_URL}: {response.status_code}, {response.text}")
 
         if len(coordinates_log)!=0:
             last_coordinate_data = coordinates_log[-1]  # 리스트의 마지막 요소 가져오기    
@@ -102,6 +162,35 @@ def receive_coordinates():
             coordinates_log.append({'latitude': latitude, 'longitude': longitude, 'speed': speed})
             print(f"Received coordinate: Latitude: {latitude}, Longitude: {longitude}, speed: {parsed_data}")
             print(coordinates_log)
+
+            gps_container_be=f"{IN_CSE_URL}/TinyIoT/becane/gps"
+            headers = {
+                "Content-Type": "application/json",
+                "X-M2M-RVI": "3",
+                "X-M2M-Origin": "CAdmin"
+            }
+            payload = {'latitude': latitude, 'longitude': longitude}
+            
+            response = requests.post(gps_container_be, headers=headers, json=payload)
+            if response.ok:
+                print(f"Successfully sent GPS data to {IN_CSE_URL}.")
+            else:
+                print(f"Failed to send GPS data to {IN_CSE_URL}: {response.status_code}, {response.text}")
+
+            speed_container=f"{IN_CSE_URL}/TinyIoT/becane/speed"
+            headers = {
+                "Content-Type": "application/json",
+                "X-M2M-RVI": "3",
+                "X-M2M-Origin": "CAdmin"
+            }
+            payload = {'speed': speed}
+            
+            response = requests.post(speed_container, headers=headers, json=payload)
+            if response.ok:
+                print(f"Successfully sent speed data to {IN_CSE_URL}.")
+            else:
+                print(f"Failed to send speed data to {IN_CSE_URL}: {response.status_code}, {response.text}")
+
             return jsonify({'status': 'Coordinate received', 'parsed_data': parsed_data}), 200
         except subprocess.CalledProcessError as e:
             print(f"Error executing parse_gps.py: {e}")
